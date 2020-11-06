85052118efb962b9ac8722d1ccf499660c316489
sourov <1705072@ugrad.cse.buet.ac.bd>
2020-11-07 00:54:22 +0600

    yes

--- hms/conf.py
+++ hms/conf.py
@@ -3,7 +3,8 @@ user_id = ''
 username = ''
 name = ''
 email = ''
-role = ''
+role = 'director'
+#roles can be customer, staff, manager, director, (admin?)
 
 def getuser():
-    return {'user_id' : user_id, 'username' : username, 'name' : name, 'email' : email}
+    return {'user_id' : user_id, 'username' : username, 'name' : name, 'email' : email, 'role' : role}
