cee104edaf9933448b2f291ea0bd4e844e973d3b
sourov <1705072@ugrad.cse.buet.ac.bd>
2020-11-12 20:20:02 +0600

    reservation and service form

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='resHome'),
+    path('createReserv/', views.reserv, name='createreservation'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -4,3 +4,6 @@ from hms import conf
 # Create your views here.
 def index(request):
     return render(request, 'reservation/index.html', {'login' : conf.login, 'user' : conf.getuser()})
+
+def reserv(request):
+    return render(request, 'reservation/createReserv.html', {'login' : conf.login, 'user' : conf.getuser()})
